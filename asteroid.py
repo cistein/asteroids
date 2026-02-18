44d3c181319cea6f02460056668fac1dfdfb67ea
cistein <calebstein1@gmail.com>
2026-02-18 16:17:21 -0600

    I: Asteroid splitting added

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +14,17 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        new_asteroid_vector = self.velocity.rotate(angle)
+        new_asteroid_2_vector = self.velocity.rotate(-angle)
+        self.radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, self.radius)
+        asteroid1.velocity = new_asteroid_vector * 1.2
+        asteroid2 = Asteroid(self.position.x, self.position.y, self.radius)
+        asteroid2.velocity = new_asteroid_2_vector
