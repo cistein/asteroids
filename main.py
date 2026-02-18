5f743605eaabb34f2bb2115a95e2ed5f49c7da23
cistein <calebstein1@gmail.com>
2026-02-18 13:04:26 -0600

    C: Added player polygon

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -12,6 +13,7 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -19,9 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
+        
+        
     
 
 
